83739bed05ee5e242ff81bf6e6c315794e05dfc9
PlanC <planc2333@outlook.com>
2020-02-22 08:45:47 +0800

    Add files via upload

---
+++ terminal/install.bat
@@ -0,0 +1,3 @@
+start python-3.4.4.amd64.msi
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
---
+++ terminal/py-qrcode.py
@@ -0,0 +1,64 @@
+import sys
+import cv2
+import time
+import imutils
+import openpyxl
+from PIL import Image
+from pyzbar.pyzbar import decode, ZBarSymbol
+
+# @function 'detect_qr' detect and decode qrcode from frame using pyzbar lib
+# @param 'inputFrame' type <class 'numpy.ndarray'>
+# @return if detected type 'bool'
+def detect_qr(inputFrame):
+	img = Image.fromarray(inputFrame)
+	decodedImg = decode(img, symbols=[ZBarSymbol.QRCODE])
+
+	if len(decodedImg) > 0:
+		decodedBytes = decodedImg[0].data
+		stringData = decodedBytes.decode("utf-8")
+		print("QRCode content:")
+		print(stringData)
+		write_excel(stringData)
+		# write into a file named by date
+
+		return True
+	else:
+		return False
+
+# open excel(.xlsx) file, if not exist create
+def write_excel(inputData):
+	filename = time.strftime("%Y-%m-%d", time.localtime()) + ".csv"
+	with open(filename, 'a') as file:
+		file.write(inputData.replace("_", ",") + ",\n")
+
+while 1:
+	if len(sys.argv)>1:
+		args = sys.argv[1:]
+
+		if args[0] == "-f":
+			inputImage = cv2.imread(args[1])
+			frame = imutils.resize(inputImage, width=400)
+			print(detect_qr(frame))
+		else:
+			print("An error occurred when parsing arguments")
+	else:
+		cam = cv2.VideoCapture(0)
+		qrDecoder = cv2.QRCodeDetector()
+		cv2.namedWindow("QRCode detector")
+		img_counter = 0
+
+		# waiting to capture
+		print("Waiting to scan a QRCode")
+		undetected = False
+		while not(undetected):
+			ret, frame = cam.read()
+			cv2.imshow("test", frame)
+			if not ret:
+				break
+
+			resizedFrame = imutils.resize(frame, width=400)
+			cv2.waitKey(1)
+			undetected = detect_qr(resizedFrame)
+		
+		cam.release()
+cv2.destroyAllWindows()
